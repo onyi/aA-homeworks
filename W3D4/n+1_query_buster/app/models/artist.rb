75c2aee979ced731048389ac61c290cf39c1116c
onyi <onyicho@gmail.com>
2019-05-30 00:15:42 -0700

    Attempt to solve better query

---
+++ W3D4/n+1_query_buster/app/models/artist.rb
@@ -0,0 +1,31 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artist < ApplicationRecord
+  has_many :albums,
+    class_name: 'Album',
+    foreign_key: :artist_id,
+    primary_key: :id
+
+  def n_plus_one_tracks
+    albums = self.albums
+    tracks_count = {}
+    albums.each do |album|
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+  end
+
+  def better_tracks_query
+    # TODO: your code here
+    Album.select('albums.*, COUNT(*) as tracks_count').joins(:tracks).where('albums.artist_id = ?', id).group('albums.id')
+  end
+end
