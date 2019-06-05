e27e51736f95bd1be7c04bdd84f50c4bfbdf115c
onyi <onyicho@gmail.com>
2019-06-04 20:14:46 -0700

    Fix missing indices and constraints for Users table

---
+++ W4D3/rails_auth/db/migrate/20190605030713_add_user_indices.rb
@@ -0,0 +1,7 @@
+class AddUserIndices < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :username, unique: true
+    add_index :users, :password_digest
+    add_index :users, :session_token, unique: true
+  end
+end
---
+++ W4D3/rails_auth/db/migrate/20190605031013_add_user_contraints.rb
@@ -0,0 +1,7 @@
+class AddUserContraints < ActiveRecord::Migration[5.2]
+  def change
+    change_column_null :users, :username, false
+    change_column_null :users, :password_digest, false
+    change_column_null :users, :session_token, false
+  end
+end
