3f7c31294bd74f099671c3ac06e4e6657c32d1d7
onyi <onyicho@gmail.com>
2019-06-20 10:10:01 -0700

    Allow user to enter element in form and prepend it to the Unordered List

--- W6D4/script.js
+++ W6D4/script.js
@@ -19,8 +19,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding SF places as list items
 
   // --- your code here!
-  document.getElementsByClassName("favorite-submit");
-  document.getElementById("restaurants")
+  const submit = document.getElementsByClassName("favorite-submit");
+  const restaurants = document.getElementById("restaurants");
+  const input = document.getElementsByClassName("favorite-input")[0];
+  const favPlace = document.getElementsByClassName("add-favorite-place");
+  console.log(favPlace);
+  favPlace[0].addEventListener("submit", (e) => {
+    e.preventDefault();
+    // console.log(`Submit event triggered, e: ${e}`);
+    let place = input.value;
+    console.log(`Values: ${place}`);
+    const li = document.createElement("li");
+    li.textContent = place;
+    restaurants.prepend(li);
+  }); 
+
+
 
 
   // adding new photos
