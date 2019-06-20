6d6fa8292f82410355f878f30d4f0bdb170667bc
onyi <onyicho@gmail.com>
2019-06-20 10:23:58 -0700

    Allow user to add photos to the photo list

--- W6D4/script.js
+++ W6D4/script.js
@@ -40,6 +40,27 @@ document.addEventListener("DOMContentLoaded", () => {
   // adding new photos
 
   // --- your code here!
+  const showPhotoButton = document.getElementById("photo-show-button");
+  showPhotoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const showPhotoDiv = document.getElementsByClassName("photo-form-container")[0];
+    if(showPhotoDiv.classList.contains("hidden"))
+      showPhotoDiv.classList.remove("hidden");
+  })
+  const showPhotoForm = document.getElementById("show-photo-form");
+  showPhotoForm.addEventListener("submit", e => {
+    e.preventDefault();
+  })
+  const showPhotoSubmit = document.getElementsByClassName("photo-url-submit")[0];
+  showPhotoSubmit.addEventListener("click", (e) => {
+    const photoUrlInput = document.getElementsByClassName("photo-url-input")[0];
+    const photos = document.getElementsByClassName("dog-photos")[0];
+    let li = document.createElement("li");
+    let img = document.createElement("img");
+    img.setAttribute("src", photoUrlInput.value);
+    li.append(img)
+    photos.append(li);
+  });
 
 
 
