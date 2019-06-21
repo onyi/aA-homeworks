0921c469df5ed999509fd39ac9340924c5598b7b
onyi <onyicho@gmail.com>
2019-06-20 21:34:17 -0700

    Change index.html to load the correct webpack generated JS file, as well as fix a typo

--- W6D5/skeleton/webpack.config.js
+++ W6D5/skeleton/webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: "./entry.jsx",
   output: {
-    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    // path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
   devtool: 'source-map',
