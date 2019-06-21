49cfd2ab21f612f1c61138e87169624b14570fb1
onyi <onyicho@gmail.com>
2019-06-20 23:01:41 -0700

    Remove previous jsx file and replace with calculator component

---
+++ W6D5/calculator/calculator.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props){
+    super(props);
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World from React Component!!!</h1>
+      </div>
+    )
+  };
+}
+
+export default Calculator;
--- W6D5/calculator/webpack.config.js
+++ W6D5/calculator/webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./entry.jsx",
+  entry: "./app.jsx",
   output: {
     // path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
