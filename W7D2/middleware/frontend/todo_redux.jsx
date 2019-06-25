5f8f2d0e5e31d3043ed3e4262ef9244c48e4e2f0
onyi <onyicho@gmail.com>
2019-06-24 21:20:57 -0700

    Commit middleware related changes

---
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+
+  
+  //Part 1
+  // store.dispatch = addLoggingToDispatch(store);
+
+  //Part 2
+  store = applyMiddleWares(store, addLoggingToDispatch);
+
+
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+
+//Part 1
+// const addLoggingToDispatch = (store) => {
+//   let origDispatch = store.dispatch;
+//   return (action) => {
+//     console.log(store.getState());
+//     console.log(action);
+//     origDispatch(action);
+//     console.log(store.getState());
+//   }
+// }
+
+//Part 2
+const addLoggingToDispatch = store => next => action => {
+  console.log(`Previous State:${store.getState()}`);
+  console.log(action);
+  next(action);
+  console.log(`New State: ${store.getState()}`);
+}
+
+const applyMiddleWares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach(middleware => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch} );
+
+};
